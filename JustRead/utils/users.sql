eab1f353fbf058322d43b33dae3187dff59b504d
Haulik <thomasbarchager@hotmail.dk>
2023-06-06 14:01:54 +0200

    Users

--- JustRead/utils/users.sql
+++ JustRead/utils/users.sql
@@ -1,41 +1,55 @@
 DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE IF NOT EXISTS Users(
-	pk serial not null PRIMARY KEY,
+	id serial not null PRIMARY KEY,
 	user_name varchar(50) UNIQUE,
     full_name varchar(50),
-	password varchar(120)
+	password varchar(120),
+    address varchar(200),
 );
 
-CREATE INDEX IF NOT EXISTS users_index
-ON Users (pk, user_name);
+--CREATE INDEX IF NOT EXISTS users_index
+--ON Users (id, user_name);
 
 DELETE FROM Users;
 
-DROP TABLE IF EXISTS Farmers CASCADE;
+DROP TABLE IF EXISTS BookStore CASCADE;
 
-CREATE TABLE IF NOT EXISTS Farmers(
-    PRIMARY KEY(pk)
+CREATE TABLE IF NOT EXISTS BookStore(
+    PRIMARY KEY(id)
 ) INHERITS (Users);
 
-CREATE INDEX IF NOT EXISTS farmers_index
-ON Farmers (pk, user_name);
+--CREATE INDEX IF NOT EXISTS bookstore_index
+--ON BookStore (id, user_name);
 
-DELETE FROM Farmers;
+DELETE FROM BookStore;
 
-INSERT INTO Farmers(user_name, full_name, password)
-VALUES ('farmer', 'Farmer', 'pass');
+INSERT INTO BookStore(user_name, full_name, password, address)
+VALUES ('Bookstore1', 'Saxo', 'pass', 'Universitetsparken 34');
 
 DROP TABLE IF EXISTS Customers;
 
 CREATE TABLE IF NOT EXISTS Customers(
-    PRIMARY KEY(pk)
+    PRIMARY KEY(id)
 ) INHERITS (Users);
 
-CREATE INDEX IF NOT EXISTS customers_index
-ON Customers (pk, user_name);
+--CREATE INDEX IF NOT EXISTS customers_index
+--ON Customers (id, user_name);
 
 DELETE FROM Customers;
 
-INSERT INTO Customers(user_name, full_name, password)
-VALUES ('customer', 'Customer', 'pass');
+INSERT INTO Customers(user_name, full_name, password, address)
+VALUES ('customer', 'Customer', 'pass', 'Nørre alle 63');
+
+
+CREATE TABLE IF NOT EXISTS Courier(
+    PRIMARY KEY(id)
+) INHERITS (Users);
+
+--CREATE INDEX IF NOT EXISTS customers_index
+--ON Customers (id, user_name);
+
+DELETE FROM Courier;
+
+INSERT INTO Courier(user_name, full_name, password, address)
+VALUES ('courier', 'Courier', 'pass', 'Nørre alle 34');
