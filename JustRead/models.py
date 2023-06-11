8f0098ebc8811bea6afe41ef9f71069728205363
AgnesSophie <agnes@galster.dk>
2023-06-11 14:34:22 +0200

    minus SQLAlchemy

--- JustRead/models.py
+++ JustRead/models.py
@@ -1,16 +1,8 @@
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
 from psycopg2 import sql
 from typing import Dict
 from JustRead import login_manager, db_cursor, conn, app
 
-
-Base = declarative_base()
-
-from sqlalchemy.ext.declarative import declared_attr
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -21,7 +13,6 @@ def load_user(user_id):
 
     db_cursor.execute(user_sql, (int(user_id),))
     return User(db_cursor.fetchone()) if db_cursor.rowcount > 0 else None
-
 
 
 class ModelUserMixin(UserMixin):
@@ -41,7 +32,7 @@ class User(ModelUserMixin, ModelMixin):
         self.user_name = user_data.get('user_name')
         self.password = user_data.get('password')
         self.address = user_data.get('address')
-        
+
     def check_password(self, password):
         return password == self.password
 
@@ -59,21 +50,20 @@ class Courier(User):
 class PublishingHouse(User):
     pass
 
+
 class Customer(User):
     def __init__(self, user_data: Dict):
         super().__init__(user_data)
 
 
-
-class Order(Base):
-    __tablename__ = 'orders'
-
-    oid = Column(Integer, primary_key=True)
-    address = Column(String(30))
-    date = Column(String(50))
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship('User')
-    # Additional columns specific to Order
+class Order:
+    def __init__(self, oid, address, date, user_id):
+        self.oid = oid
+        self.address = address
+        self.date = date
+        self.user_id = user_id
+        self.user = None
+        # Additional attributes specific to Order
 
 
 class Book(ModelMixin):
@@ -91,9 +81,8 @@ class Book(ModelMixin):
         # Additional attributes specific to Book
 
 
-class Author(Base):
-    __tablename__ = 'authors'
-
-    pid = Column(Integer, primary_key=True)
-    name = Column(String(30))
-    # Additional columns specific to Author
+class Author:
+    def __init__(self, pid, name):
+        self.pid = pid
+        self.name = name
+        # Additional attributes specific to Author
